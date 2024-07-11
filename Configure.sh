38dc0ef31f51e1ae4df1e3d5e6653aeef48041e2
Marcel Pi <25649656+MarcelPiNacy@users.noreply.github.com>
2024-07-11 17:29:31 +0200

    Update bash script.

--- Configure.sh
+++ Configure.sh
@@ -6,7 +6,7 @@ else
 	git clone -b develop https://github.com/carla-simulator/RobotecGPULidar.git RobotecGPULidar
 fi
 cd RobotecGPULidar
-python setup.py --install-deps
+python3 setup.py --install-deps
 cmake -B Build -G Ninja -S . -DCMAKE_INSTALL_PREFIX=$PWD/Install -DCMAKE_BUILD_TYPE=Release -DRGL_BUILD_TESTS=OFF -DRGL_BUILD_TAPED_TESTS=OFF -DRGL_BUILD_TOOLS=OFF -DRGL_BUILD_STATIC=ON
 cmake --build Build
 cd ..
